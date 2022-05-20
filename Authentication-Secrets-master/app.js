8387f273887444c3465fa69f4ce073f645d2939e
Julius170 <juliusyoola5@gmail.com>
2022-05-20 11:08:26 +0100

    Level 6 OAuth

--- Authentication-Secrets-master/app.js
+++ Authentication-Secrets-master/app.js
@@ -11,7 +11,8 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
-
+const GoogleStratergy = require('passport-google-oauth20').Strategy;
+const findOrCreate = reqiure("mongoose-findorcreate");
 
 
 
@@ -40,10 +41,12 @@ mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema ({
   email: String,
-  password: String
+  password: String,
+  googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 // userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedField: ['password'] });
 
@@ -51,14 +54,47 @@ const User = new mongoose.model('User', userSchema);
 
 passport.use(User.createStratergy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById,(id, function(err, user) {
+    done(err, user);
+  })
+});
+
+passport.use(new GoogleStratergy({
+  clientID:process.env.client_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: "http://www.example.com/auth/google/secrets",
+  userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+},
+function(accessToken, refreshToken, profile, cb) {
+  User.findOrCreate({ googleId:profile.id}, function(err, user){
+    return cb(err, user);
+  });
+}))
+
 
 
 app.get('/', function(req, res){
   res.render('home');
 
 });
+
+
+app.get("/auth/google", function(req, res) {
+  passport.authenticate("google", { scope: ["profile"] })
+});
+
+app.get("/auth/google/secrets",
+  passport.authenticate("google", {faliureRedirect: "login" }),
+  function(req, res) {
+    // Successful authentication redirect home
+    res.redirect("/secrets")
+  }
+);
 
 
 app.get('/login', function(req, res){
